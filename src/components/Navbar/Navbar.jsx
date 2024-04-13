c663b9e4985b5e1b6297ffbd98d6dc8d7b853bca
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-13 08:11:10 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
+
 const Navbar =() => {
     const [sticky,setSticky] = useState(false);
 
@@ -14,12 +15,13 @@ const Navbar =() => {
         <nav className={`container ${sticky? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo'/>
             <ul>
-                <li>Home</li>
+                <li></li>
                 <li>Program</li>
                 <li>About Us</li>
                 <li>Campus</li>
                 <li><button className='btn'>Contact Us</button></li>
             </ul>
+            
         </nav>
     )
 }
