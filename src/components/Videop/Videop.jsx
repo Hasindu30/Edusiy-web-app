dd2e9b3930343d17029687951946ac2d4c2228f0
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-14 00:36:03 +0530

    modified:   src/components/About/About.css 	modified:   src/components/Videop/Videop.jsx

--- src/components/Videop/Videop.jsx
+++ src/components/Videop/Videop.jsx
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react'
 import'./Videop.css'
 import video from'../../assets/video.mp4'
+
 const Videop = ({playState,SetPlayState}) => {
-    const player = useRef(null);
+    const player =useRef(null);
 
     const closePlayer = (e)=>{
       if(e.target === player.current){
-          SetPlayState(false);
+            SetPlayState(false);
       }
     }
 
