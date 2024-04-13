429113ca66301587735d87131442b234cda93201
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-14 00:24:10 +0530

    update the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/programs'
@@ -7,7 +7,12 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import Videop from './components/Videop/Videop'
+
 const App = () => {
+
+  const [playState,setPlayState] =useState(false);
+  
   return (
     <div>
       <Navbar/>
@@ -15,13 +20,14 @@ const App = () => {
       <div className="container">
         <Title subTitle='OUR PROGRAM' title='What we Offer' />
       <Programs/>
-      <About/>
+      <About setPlayState = {setPlayState}/>
       <Title subTitle='Gallery' title='Campus Photos'/>
       <Campus/>
       <Title subTitle='Contact Us' title='Get in Touch'/>
       <Contact/>
       <Footer/>
       </div>
+      <Videop playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
---
+++ src/components/Videop/Videop.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react'
+import'./Videop.css'
+import video from'../../assets/video.mp4'
+const Videop = ({playState,SetPlayState}) => {
+    const player = useRef(null);
+
+    const closePlayer = (e)=>{
+      if(e.target === player.current){
+          SetPlayState(false);
+      }
+    }
+
+  return (
+    <div className={`Video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
+      <video src={video}autoPlay muted controls></video>
+    </div>
+  )
+}
+
+export default Videop
